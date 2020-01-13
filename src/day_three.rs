c6b0e1e745979bed7747f081cf4e5b94ebaee922
JLDLaughlin <jessica@materialize.io>
2020-01-13 14:23:07 -0500

    Day three, second star

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,5 +1,5 @@
 use failure;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 static DATA_FILEPATH: &str = "src/data/day_three.txt";
@@ -19,8 +19,8 @@ pub fn first_star() -> Result<i64, failure::Error> {
 // Broken out to simplify testing.
 fn first_star_inner(first_input: &str, second_input: &str) -> Result<i64, failure::Error> {
     // Get all coordinates.
-    let first_coords = get_all_coordinates_from_input(first_input).unwrap();
-    let second_coords = get_all_coordinates_from_input(second_input).unwrap();
+    let (first_coords, _) = get_all_coordinates_from_input(first_input).unwrap();
+    let (second_coords, _) = get_all_coordinates_from_input(second_input).unwrap();
 
     // Get min distance, if any.
     let mut min_distance = std::i64::MAX;
@@ -47,12 +47,50 @@ fn get_manhattan_distance(coordinate: &(i64, i64)) -> i64 {
     coordinate.0.abs() + coordinate.1.abs()
 }
 
-fn get_all_coordinates_from_input(input: &str) -> Result<HashSet<(i64, i64)>, failure::Error> {
-    let mut coordinates = HashSet::new();
+pub fn second_star() -> Result<i64, failure::Error> {
+    // Parse text file.
+    let text = fs::read_to_string(DATA_FILEPATH).unwrap();
+    let split: Vec<&str> = text.split("\n").collect();
+    if split.len() != 2 {
+        failure::bail!("Expected two sets of coordinates, found {}", split.len());
+    }
+
+    // Call and return result of inner function.
+    second_star_inner(split[0], split[1])
+}
+
+fn second_star_inner(first_input: &str, second_input: &str) -> Result<i64, failure::Error> {
+    // Get all coordinates and steps.
+    let (first_coords, first_steps_map) = get_all_coordinates_from_input(first_input).unwrap();
+    let (second_coords, second_steps_map) = get_all_coordinates_from_input(second_input).unwrap();
+
+    // Get min of steps for both wires to reach coord, if exists.
+    let mut min_distance = std::i64::MAX;
+    for coord in first_coords.intersection(&second_coords) {
+        let first_steps = first_steps_map.get(coord).unwrap();
+        let second_steps = second_steps_map.get(coord).unwrap();
+        if first_steps + second_steps != 0 && first_steps + second_steps < min_distance {
+            min_distance = first_steps + second_steps;
+        }
+    }
+
+    Ok(min_distance)
+}
+
+fn get_all_coordinates_from_input(
+    input: &str,
+) -> Result<(HashSet<(i64, i64)>, HashMap<(i64, i64), i64>), failure::Error> {
+    let mut coordinates: HashSet<(i64, i64)> = HashSet::new();
+    let mut coords_to_steps: HashMap<(i64, i64), i64> = HashMap::new();
 
     let mut last_x = 0;
     let mut last_y = 0;
+    // Number of steps to reach coordinate (last_x, last_y).
+    let mut steps: i64 = 0;
+
     coordinates.insert((last_x, last_y));
+    coords_to_steps.insert((last_x, last_y), steps);
+    steps += 1;
 
     for i in input.split(",") {
         let direction: &str = &i[..1];
@@ -63,7 +101,9 @@ fn get_all_coordinates_from_input(input: &str) -> Result<HashSet<(i64, i64)>, fa
                 let mut y = last_y + 1;
                 while y <= new_y {
                     coordinates.insert((last_x, y));
+                    coords_to_steps.insert((last_x, y), steps);
                     y += 1;
+                    steps += 1;
                 }
                 last_y = new_y;
             }
@@ -72,7 +112,9 @@ fn get_all_coordinates_from_input(input: &str) -> Result<HashSet<(i64, i64)>, fa
                 let mut y = last_y - 1;
                 while y >= new_y {
                     coordinates.insert((last_x, y));
+                    coords_to_steps.insert((last_x, y), steps);
                     y -= 1;
+                    steps += 1;
                 }
                 last_y = new_y;
             }
@@ -81,7 +123,9 @@ fn get_all_coordinates_from_input(input: &str) -> Result<HashSet<(i64, i64)>, fa
                 let mut x = last_x + 1;
                 while x <= new_x {
                     coordinates.insert((x, last_y));
+                    coords_to_steps.insert((x, last_y), steps);
                     x += 1;
+                    steps += 1;
                 }
                 last_x = new_x;
             }
@@ -90,14 +134,16 @@ fn get_all_coordinates_from_input(input: &str) -> Result<HashSet<(i64, i64)>, fa
                 let mut x = last_x - 1;
                 while x >= new_x {
                     coordinates.insert((x, last_y));
+                    coords_to_steps.insert((x, last_y), steps);
                     x -= 1;
+                    steps += 1;
                 }
                 last_x = new_x;
             }
             _ => failure::bail!("Invalid instruction type: {}", i),
         }
     }
-    Ok(coordinates)
+    Ok((coordinates, coords_to_steps))
 }
 
 #[cfg(test)]
@@ -126,7 +172,7 @@ mod tests {
     }
 
     #[test]
-    fn test() {
+    fn test_day_one() {
         let test_cases = vec![
             TestCase::new("R8,U5,L5,D3", "U7,R6,D4,L4", 6),
             TestCase::new(
@@ -145,6 +191,29 @@ mod tests {
             assert_eq!(
                 case.expected_distance,
                 first_star_inner(case.first_input, case.second_input).unwrap()
+            )
+        }
+    }
+
+    #[test]
+    fn test_day_two() {
+        let test_cases = vec![
+            TestCase::new(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83",
+                610,
+            ),
+            TestCase::new(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                410,
+            ),
+        ];
+
+        for case in test_cases {
+            assert_eq!(
+                case.expected_distance,
+                second_star_inner(case.first_input, case.second_input).unwrap()
             )
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(dead_code)] // crate attribute. in main.rs because this is a binary.
 mod day_one;
-mod day_two;
 mod day_three;
+mod day_two;
 
 fn main() {
     //    println!("day_two first_star: {}", day_two::first_star());
     //    println!("day_two second_star: {}", day_two::second_star());
-    println!("day_three first star: {}", day_three::first_star().unwrap());
+    //    println!("day_three first star: {}", day_three::first_star().unwrap());
+    println!(
+        "day_three first star: {}",
+        day_three::second_star().unwrap()
+    );
 }
